bc0a29b6321b7247be3002ec5c9f9a1a610e4063
olliestyle <olliestyle1989@gmail.com>
2021-09-12 15:37:20 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
@@ -2,6 +2,10 @@ package ru.job4j.concurrent;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -52,5 +56,38 @@ public class SimpleBlockingQueueTest {
         producer3.join();
         producer4.join();
         assertThat(queue.size(), is(4));
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 5).forEach(value -> {
+                    try {
+                        queue.offer(value);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                })
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (queue.size() != 0 || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
     }
 }
