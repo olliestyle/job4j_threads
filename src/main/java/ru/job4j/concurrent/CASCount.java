0d6b71bf5ced2ac793d8f5492b25d4ed496f0e9e
olliestyle <olliestyle1989@gmail.com>
2021-09-14 09:50:47 +0300

    0. CAS - операции [#6859].1

--- src/main/java/ru/job4j/concurrent/CASCount.java
+++ src/main/java/ru/job4j/concurrent/CASCount.java
@@ -6,49 +6,52 @@ import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
 public class CASCount {
-    private final AtomicReference<Integer> count = new AtomicReference<>();
+    private final AtomicReference<Integer> count;
+
+    public CASCount(Integer count) {
+        this.count = new AtomicReference<>(count);
+    }
 
     public void increment() {
         Integer value = count.get();
-        if (count.get() == null) {
-            count.set(0);
+        while (!count.compareAndSet(value, value + 1)) {
             value = count.get();
         }
-        count.compareAndSet(value, value + 1);
     }
 
     public int get() {
-        Integer toReturn = count.get();
-        if (toReturn == null) {
-            throw new IllegalStateException("No value to get");
-        }
-        return toReturn;
+        return count.get();
     }
 
     public static void main(String[] args) throws InterruptedException {
-        CASCount casCount = new CASCount();
+        CASCount casCount = new CASCount(0);
         Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 100000000; i++) {
+            for (int i = 0; i < 1000000; i++) {
                casCount.increment();
            }
         });
+        Thread thread6 = new Thread(() -> {
+            for (int i = 0; i < 1000000; i++) {
+                casCount.increment();
+            }
+        });
         Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 5; i++) {
                 System.out.println(casCount.get() + " Thread111");
             }
         });
         Thread thread3 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 5; i++) {
                 System.out.println(casCount.get() + " Thread222");
             }
         });
         Thread thread4 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 5; i++) {
                 System.out.println(casCount.get() + " Thread333");
             }
         });
         Thread thread5 = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 5; i++) {
                 System.out.println(casCount.get() + " Thread444");
             }
         });
@@ -57,10 +60,12 @@ public class CASCount {
         thread3.start();
         thread4.start();
         thread5.start();
+        thread6.start();
         thread1.join();
         thread2.join();
         thread3.join();
         thread4.join();
         thread5.join();
+        thread6.join();
     }
 }
