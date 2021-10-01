7eb7a65157df072e84ed8125d629b55e84280d58
olliestyle <olliestyle1989@gmail.com>
2021-10-01 12:11:01 +0300

    3.2. Откат изменений. [#307144]

---
+++ src/test/java/ru/job4j/concurrent/Foo.java
@@ -0,0 +1,10 @@
+package ru.job4j.concurrent;
+
+public class Foo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("soft");
+        System.out.println("reset");
+    }
+}
